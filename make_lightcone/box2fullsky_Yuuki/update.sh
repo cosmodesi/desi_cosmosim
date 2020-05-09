0458b92404e835c3655df49c6f9ccbf92f458227
chia-hsun-chuang <57546434+chia-hsun-chuang@users.noreply.github.com>
2020-05-09 01:59:59 -0700

    arrange make_lightcone and clustering directories

--- make_lightcone/box2fullsky_Yuuki/update.sh
+++ make_lightcone/box2fullsky_Yuuki/update.sh
@@ -3,9 +3,9 @@
 #script credit: https://www.sourcefield.nl/post/git-subtree-survival-tips/
 #
 REF=${1-master} # branch or tag; defaults to 'master' if parameter 1 not present
-REMOTE=Ginevra_CF # just a name to identify the remote
-REPO=https://github.com/gfavole/multi2pcf.git # replace this with your repository URL
-FOLDER=clustering/Ginevra_CF/multi2pcf # where to mount the subtree
+REMOTE=yuuki # just a name to identify the remote
+REPO=https://github.com/yomori/gallightcone.git # replace this with your repository URL
+FOLDER=make_lightcone/box2fullsky_Yuuki/gallightcone # where to mount the subtree
 
 git remote add $REMOTE --no-tags $REPO
 if [[ -d $FOLDER ]]; then # update the existing subtree
