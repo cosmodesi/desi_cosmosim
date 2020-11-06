c1e8028e3eebae4b31a2919032b39706845bda0e
chia-hsun-chuang <57546434+chia-hsun-chuang@users.noreply.github.com>
2020-11-06 15:41:31 -0800

    Create update.sh

---
+++ clustering/HMF_and_CF_cosmosim/update.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#Usage: ./update.sh [<branch or tag (defaults to 'master')>]
+#script credit: https://www.sourcefield.nl/post/git-subtree-survival-tips/
+#
+REF=${1-master} # branch or tag; defaults to 'master' if parameter 1 not present
+REMOTE=remote # just a name to identify the remote
+REPO=https://github.com/cejgrove/HMF_CF.git # replace this with your repository URL
+FOLDER=clustering/HMF_and_CF_cosmosim/HMF_CF # where to mount the subtree
+
+git remote add $REMOTE --no-tags $REPO
+if [[ -d $FOLDER ]]; then # update the existing subtree
+    git subtree pull $REMOTE $REF --prefix=$FOLDER --squash -m "Merging '$REF' into '$FOLDER'"
+else # add the subtree
+    git subtree add  $REMOTE $REF --prefix=$FOLDER --squash -m "Merging '$REF' into '$FOLDER'"
+fi
+git remote remove $REMOTE
